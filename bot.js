8d3ef8b8872e050d7d93192b19ca3bc32a200440
Juri Lelli <juri@localhost-live.home>
2018-07-25 22:21:56 +0200

    jlelli mods

--- bot.js
+++ bot.js
@@ -1,27 +1,33 @@
 // Authorized users, replace with your real IDs
 var authorized_users = [
-  111111111,
+  "username1",
+  "username2",
 ];
 
 // Include required libraries
-var sensorLib = require('node-dht-sensor');
-var Bot = require('node-telegram-bot');
+//var sensorLib = require('node-dht-sensor');
+var telegram = require('telegram-bot-api');
+var shell = require('shelljs');
 
 // Initialize relay board (using onoff library)
-var Gpio = require('onoff').Gpio,
-  relay1 = new Gpio(2, 'out'),
-  relay2 = new Gpio(3, 'out');
+//var Gpio = require('onoff').Gpio,
+//  relay1 = new Gpio(2, 'out'),
+//  relay2 = new Gpio(3, 'out');
 
 // Turn both the relays off
-relay1.writeSync(0);
-relay2.writeSync(0);
+//relay1.writeSync(0);
+//relay2.writeSync(0);
 
 // Initialize DHT11 sensor
-sensorLib.initialize(11, 4);
+//sensorLib.initialize(11, 4);
 
 // Initialize and start Telegram BOT (insert your real token)
-var bot = new Bot({
-  token: '0123456789abcdef0123456789abcdef0123456789ab'
+var bot = new telegram({
+  token: '<obtain through BotFather>',
+  updates : {
+    enabled: true,
+    get_interval: 2000
+  }
 });
 
 // Attach event on every received message 
@@ -30,87 +36,87 @@ bot.on('message', function (message) {
 });
 
 // Start the bot
-bot.start();
 console.log("BOT ready!");
+
+var lights_status = {
+	studio: "OFF"
+};
 
 // Function that handles a new message
 function parseMessage(message) {
 
-  if(!isAuthorized(message.from.id)) return;
+  if(!isAuthorized(message.from.username)) {
+      bot.sendMessage({
+        chat_id: message.chat.id,
+        text: "Go away " + message.from.first_name + " " + message.from.last_name + " (AKA " + message.from.username + ") ! \nYou are not authorized to enter these premises!",
+      });
+      console.log(message.from.username + " tried to enter the premises!!!");
+      return;
+  }
 
   switch(true) {
-  
-    case message.text == "/gettemp":
+
+    case message.text == "/start":
       bot.sendMessage({
         chat_id: message.chat.id,
-        text: 'Actual temperature: ' + sensorLib.read().temperature.toFixed(0) + '°C',
+        text: "Welcome " + message.from.first_name + " " + message.from.last_name + " to juril Home Automation!",
       });
+      console.log(message.from.username + " is in!");
       break;
 
-    case message.text == "/gethum":
+    case message.text == "/getlights":
       bot.sendMessage({
         chat_id: message.chat.id,
-        text: 'Actual humidity: ' + sensorLib.read().humidity.toFixed(0) + '%',
+        text: 'Lights status:\nStudio is ' + lights_status["studio"],
       });
+      console.log(message.from.username + " asked for lights status");
       break;
 
-    case message.text == "/getouts":
-      bot.sendMessage({
-        chat_id: message.chat.id,
-        text: 'Actual outputs status:\nOutput 1 is ' + relay1.readSync() + '\nOutput 2 is ' + relay2.readSync(),
-      });
-      break;
-
-    case /^\/setout1/.test(message.text):
-      var command = message.text.replace("/setout1 ", "");
+    case /^\/switchstudio/.test(message.text):
+      var command = message.text.replace("/switchstudio ", "");
       if(command.toLowerCase() == "on") {
-        relay1.writeSync(1);
+	if (shell.exec('./codesend 4543795 1 181').code != 0) {
+          bot.sendMessage({
+            chat_id: message.chat.id,
+            text: 'Oops.. something went wrong. :-(',
+          });
+	  return;
+	}
+	lights_status['studio'] = "ON";
         bot.sendMessage({
           chat_id: message.chat.id,
-          text: 'Output 1 turned ON',
+          text: 'Studio lights turned ON',
         });
+	console.log(message.from.username + " turned studio lights ON");
       } else if(command.toLowerCase() == "off") {
-        relay1.writeSync(0);
+	if (shell.exec('./codesend 4543804 1 181').code != 0) {
+          bot.sendMessage({
+            chat_id: message.chat.id,
+            text: 'Oops.. something went wrong. :-(',
+          });
+	  return;
+	}
+	lights_status['studio'] = "OFF";
         bot.sendMessage({
           chat_id: message.chat.id,
-          text: 'Output 1 turned OFF',
+          text: 'Studio lights turned OFF',
         });
+	console.log(message.from.username + " turned studio lights OFF");
       } else
         bot.sendMessage({
           chat_id: message.chat.id,
           text: 'Unknown command: ' + command,
         });    
     break;
-
-    case /^\/setout2/.test(message.text):
-      var command = message.text.replace("/setout2 ", "");
-      if(command.toLowerCase() == "on") {
-        relay2.writeSync(1);
-        bot.sendMessage({
-          chat_id: message.chat.id,
-          text: 'Output 2 turned ON',
-        });
-      } else if(command.toLowerCase() == "off") {
-        relay2.writeSync(0);
-        bot.sendMessage({
-          chat_id: message.chat.id,
-          text: 'Output 2 turned OFF',
-        });
-      } else
-        bot.sendMessage({
-          chat_id: message.chat.id,
-          text: 'Unknown command: ' + command,
-        });
-    break;
   }
 }
 
 
 // Function that checks if the user is authorized (its id is in the array)
-function isAuthorized(userid) {
+function isAuthorized(username) {
 
   for(i = 0; i < authorized_users.length; i++) 
-    if(authorized_users[i ] == userid) return true;
+    if(authorized_users[i] == username) return true;
  
   return false;
 }
