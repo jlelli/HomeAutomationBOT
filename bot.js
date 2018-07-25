a283589fc247412bfa6d29628f5b4536746e10f9
Juri Lelli <juri@localhost-live.home>
2018-07-25 23:09:27 +0200

    photos

--- bot.js
+++ bot.js
@@ -8,6 +8,7 @@ var authorized_users = [
 //var sensorLib = require('node-dht-sensor');
 var telegram = require('telegram-bot-api');
 var shell = require('shelljs');
+var dateFormat = require('dateformat');
 
 // Initialize relay board (using onoff library)
 //var Gpio = require('onoff').Gpio,
@@ -64,6 +65,19 @@ function parseMessage(message) {
       console.log(message.from.username + " is in!");
       break;
 
+    case message.text == "/photostudio":
+      if (shell.exec('<get photo with motioneye>').code != 0) {
+        return;
+      }
+      var now = new Date();
+      bot.sendPhoto({
+        chat_id: message.chat.id,
+        caption: 'Studio @ ' + dateFormat(now),
+        photo: '/tmp/snapshot.jpg'
+      });
+      console.log(message.from.username + " asked for a photo of Studio");
+      break;
+
     case message.text == "/getlights":
       bot.sendMessage({
         chat_id: message.chat.id,
@@ -72,42 +86,37 @@ function parseMessage(message) {
       console.log(message.from.username + " asked for lights status");
       break;
 
-    case /^\/switchstudio/.test(message.text):
-      var command = message.text.replace("/switchstudio ", "");
-      if(command.toLowerCase() == "on") {
-	if (shell.exec('./codesend 4543795 1 181').code != 0) {
-          bot.sendMessage({
-            chat_id: message.chat.id,
-            text: 'Oops.. something went wrong. :-(',
-          });
-	  return;
-	}
-	lights_status['studio'] = "ON";
+    case message.text == "/switchstudioon":
+      if (shell.exec('./codesend 4543795 1 181').code != 0) {
         bot.sendMessage({
           chat_id: message.chat.id,
-          text: 'Studio lights turned ON',
+          text: 'Oops.. something went wrong. :-(',
         });
-	console.log(message.from.username + " turned studio lights ON");
-      } else if(command.toLowerCase() == "off") {
-	if (shell.exec('./codesend 4543804 1 181').code != 0) {
-          bot.sendMessage({
-            chat_id: message.chat.id,
-            text: 'Oops.. something went wrong. :-(',
-          });
-	  return;
-	}
-	lights_status['studio'] = "OFF";
+        return;
+      }
+      lights_status['studio'] = "ON";
+      bot.sendMessage({
+        chat_id: message.chat.id,
+        text: 'Studio lights turned ON',
+      });
+      console.log(message.from.username + " turned studio lights ON");
+      break;
+
+    case message.text == "/switchstudiooff":
+      if (shell.exec('./codesend 4543804 1 181').code != 0) {
         bot.sendMessage({
           chat_id: message.chat.id,
-          text: 'Studio lights turned OFF',
+          text: 'Oops.. something went wrong. :-(',
         });
-	console.log(message.from.username + " turned studio lights OFF");
-      } else
-        bot.sendMessage({
-          chat_id: message.chat.id,
-          text: 'Unknown command: ' + command,
-        });    
-    break;
+        return;
+      }
+      lights_status['studio'] = "OFF";
+      bot.sendMessage({
+        chat_id: message.chat.id,
+        text: 'Studio lights turned OFF',
+      });
+      console.log(message.from.username + " turned studio lights OFF");
+      break;
   }
 }
 
